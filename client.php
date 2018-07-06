b0bb4483899a2e58d057034a1c1afb07ae93f228
Kevin Jayanthan <kevintjayan@gmail.com>
2018-07-06 15:37:44 +0530

    ShowsImages

--- client.php
+++ client.php
@@ -13,7 +13,7 @@ $accessToken = $session->getAccessToken();
 $api = new SpotifyWebAPI\SpotifyWebAPI();
 $api->setAccessToken($accessToken);
 
-$response = $api->search('attention', 'track');
+$response = $api->search('shape of you', 'track');
 $tracks = $response->tracks;
 $items = $tracks->items;
 
@@ -22,6 +22,14 @@ $items = $tracks->items;
 <?php foreach($items as $item): ?>
     <?php if(! $item->preview_url) continue; ?>
 
+    <?php $image = null; ?>
+    <?php if($item->album->images) {
+        $image = $item->album->images[0];
+    }?>
+    
+    <?php if($image): ?>
+        <img style='height: 200px; width: auto' src='<?php echo $image->url; ?>'/>
+    <?php endif; ?>
     <h4><?php echo $item->name; ?></h4>
     <audio controls>
     <source src="<?php echo $item->preview_url; ?>" type="audio/mpeg">
